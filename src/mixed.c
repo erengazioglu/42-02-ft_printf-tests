e4b3d3503027f66be229ba551eb884ed84e0b5b4
Eren Gazioglu <egaziogl@f1r4s2.clusters.42paris.fr>
2025-12-14 14:47:39 +0100

    try mixing escapes in between

--- src/mixed.c
+++ src/mixed.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 13:13:56 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/12/14 13:38:00 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/12/14 14:47:02 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@ void	test_suite(void)
 	printf("---\nMixed arguments\n");
 	fflush(stdout);
 	run_test("Test 01: %i %u %x %X", INT_MAX, INT_MAX, INT_MAX, INT_MAX);
+	run_test("Test 02: %i %u %%%x %X", INT_MAX, INT_MAX, INT_MAX, INT_MAX);
+	run_test("Test 03: %c%c %s%%%s", '4', '2', "percentage: ", "42");
 }
