56e6bb383d5c0a680d5f44be82b73f632b734496
Eren Gazioglu <egaziogl@f1r4s2.clusters.42paris.fr>
2025-12-14 14:13:03 +0100

    clean text

--- src/uint.c
+++ src/uint.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 13:50:59 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/12/14 13:54:05 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/12/14 14:11:00 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ void	test_suite(void)
 {
 	printf("---\nUNSIGNED INTS\n");
 	fflush(stdout);
-	run_test("test 1: %u", 42);
-	run_test("test 2: %u", -42);
-	run_test("test 3: %u %u", -42, 42);
-	run_test("test 4: %u %u %u", 1, 0, -1);
-	run_test("test 5: %u %u", INT_MIN, INT_MAX, UINT_MAX);
-
+	run_test("Test 1: %u", 42);
+	run_test("Test 2: %u", -42);
+	run_test("Test 3: %u %u", -42, 42);
+	run_test("Test 4: %u %u %u", 1, 0, -1);
+	run_test("Test 5: %u %u", INT_MIN, INT_MAX, UINT_MAX);
 }
