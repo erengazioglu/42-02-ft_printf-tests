51176a5052f8fc780df2ba81ecf62bb99d8025dd
Eren Gazioglu <egaziogl@f1br4s5.clusters.42paris.fr>
2025-12-17 20:00:38 +0100

    fix str bonus

--- src/manual/test1.c
+++ src/manual/test1.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 18:26:16 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/12/17 18:33:07 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/12/17 19:56:29 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,13 @@ int main(void)
 	
 	printf("Invalid conversion: %4 (without varg)\n");
 	ft_printf("Invalid conversion: %4 (without varg)\n");
+
+	printf("---\nStrings (Bonus)\n");
+	fflush(stdout);
+	ft_printf("test 1: %s", "simple\n");
+	ft_printf("test 2: %20s", "FW20\n");
+	ft_printf("test 3: %-20s", "FW20 + lalign\n");
+	ft_printf("test 4: %.16s", "just P16\n");
+	ft_printf("test 5: %20.16s", "FW20, P16\n");
+	ft_printf("test 6: %20.16s", "long string jkfgfkdjgdfkjgdfh\n");
 }
